e44dfe9f77dc9534404eab2e88666512d8d701a6
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2024-04-19 18:33:43 -0300

    Added initial visitor records

---
+++ src/main/java/com/firpy/model/MinorVisitor.java
@@ -0,0 +1,7 @@
+package com.firpy.model;
+
+import java.util.Date;
+
+public record MinorVisitor(String name, Date dateOfBirth, Visitor guardian)
+{
+}
---
+++ src/main/java/com/firpy/model/Visitor.java
@@ -0,0 +1,7 @@
+package com.firpy.model;
+
+import java.util.Date;
+
+public record Visitor(String name, Date dateOfBirth, String phoneNumber)
+{
+}
