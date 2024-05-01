5176ca54ee5949ff73e625130d1ed826793766da
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2024-05-01 15:46:29 -0300

    Used visitor data accesses on commands

--- src/main/java/com/firpy/application/commands/impls/RegisterMinorVisitorCommand.java
+++ src/main/java/com/firpy/application/commands/impls/RegisterMinorVisitorCommand.java
@@ -1,32 +1,46 @@
 package com.firpy.application.commands.impls;
 
 import com.firpy.application.commands.Command;
-import com.firpy.application.commands.arguments.impls.StringArgumentModel;
+import com.firpy.application.commands.arguments.impls.LocalDateArgumentSchema;
+import com.firpy.application.commands.arguments.impls.LongArgumentSchema;
+import com.firpy.application.commands.arguments.impls.StringArgumentSchema;
 import com.firpy.application.commands.exceptions.CommandException;
 import com.firpy.application.shell.Shell;
 import com.firpy.model.MinorVisitor;
-import com.firpy.repositories.CrudRepository;
+import com.firpy.repositories.exceptions.CheckedIllegalArgumentException;
+import com.firpy.repositories.impls.MinorVisitorDataAccess;
 import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDate;
 
 public class RegisterMinorVisitorCommand extends Command
 {
-	public RegisterMinorVisitorCommand(CrudRepository<MinorVisitor, Long> repository)
+	public RegisterMinorVisitorCommand(MinorVisitorDataAccess minorVisitorDataAccess)
 	{
 		super("register-minor-visitor", "Registers a minor visitor to the system.");
-		this.repository = repository;
-		addArgumentModels(nameArgument);
+		this.minorVisitorDataAccess = minorVisitorDataAccess;
+		addArgumentModels(nameArgument, guardianIdArgument, dateOfBirthArgument);
 	}
 
 	@Override
 	public void run(String @NotNull [] args, @NotNull Shell shell) throws CommandException
 	{
-		if (args.length != 1)
-		{
-			throw new CommandException("Invalid number of arguments.");
-		}
-		String name = nameArgument.parse(args[0]);
-	}
+		String name = nameArgument.parse(args);
+		Long guardianId = guardianIdArgument.parse(args);
+		LocalDate dateOfBirth = dateOfBirthArgument.parse(args);
+        try
+        {
+            MinorVisitor minorVisitor = minorVisitorDataAccess.registerMinorVisitor(name, dateOfBirth, guardianId);
+			shell.println("Minor visitor registered successfully: %s".formatted(minorVisitor));
+        }
+        catch (CheckedIllegalArgumentException e)
+        {
+			throw new CommandException(e.getMessage());
+        }
+    }
 
-	private final StringArgumentModel nameArgument = new StringArgumentModel("name", "The name of the visitor");
-	private final CrudRepository<MinorVisitor, Long> repository;
+	private final StringArgumentSchema nameArgument = new StringArgumentSchema("name", "The name of the minor visitor");
+	private final LongArgumentSchema guardianIdArgument = new LongArgumentSchema("guardian-id", "The id of the minor visitor's guardian");
+	private final LocalDateArgumentSchema dateOfBirthArgument = new LocalDateArgumentSchema("date-of-birth", "The date of birth of the minor visitor");
+	private final MinorVisitorDataAccess minorVisitorDataAccess;
 }
--- src/main/java/com/firpy/application/commands/impls/RegisterVisitorCommand.java
+++ src/main/java/com/firpy/application/commands/impls/RegisterVisitorCommand.java
@@ -1,7 +1,7 @@
 package com.firpy.application.commands.impls;
 
 import com.firpy.application.commands.Command;
-import com.firpy.application.commands.arguments.impls.StringArgumentModel;
+import com.firpy.application.commands.arguments.impls.StringArgumentSchema;
 import com.firpy.application.commands.exceptions.CommandException;
 import com.firpy.application.shell.Shell;
 import com.firpy.model.Visitor;
@@ -22,21 +22,15 @@ public class RegisterVisitorCommand extends Command
 	@Override
 	public void run(String @NotNull [] args, @NotNull Shell shell) throws CommandException
 	{
-		if (args.length != 2)
-		{
-			throw new CommandException("Invalid number of arguments.");
-		}
-
-		String name = nameArgument.parse(args[0]);
-		String phoneNumberValue = phoneNumber.parse(args[1]);
+		String name = nameArgument.parse(args);
+		String phoneNumberValue = phoneNumber.parse(args);
 
 		Visitor visitor = new Visitor(0, name, new Date(), phoneNumberValue);
 		repository.save(visitor);
 		shell.println("Visitor registered successfully: %s".formatted(visitor));
 	}
 
-	private final StringArgumentModel nameArgument = new StringArgumentModel("name", "The name of the visitor");
-	private final StringArgumentModel phoneNumber = new StringArgumentModel("phoneNumber", "The phone number of the visitor");
-
+	private final StringArgumentSchema nameArgument = new StringArgumentSchema("name", "The name of the visitor");
+	private final StringArgumentSchema phoneNumber = new StringArgumentSchema("phoneNumber", "The phone number of the visitor");
 	private final CrudRepository<Visitor, Long> repository;
 }
