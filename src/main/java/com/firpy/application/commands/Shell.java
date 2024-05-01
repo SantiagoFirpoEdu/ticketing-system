bfbf13d7cf5cbf62c9267c9f9a4fbe7c6fc20ee5
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-05-01 02:10:21 -0300

    Implemented simple shell wrapper

---
+++ src/main/java/com/firpy/application/commands/Shell.java
@@ -0,0 +1,57 @@
+package com.firpy.application.commands;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Shell
+{
+	public Shell()
+	{
+		commandRegistry.registerCommand(new RegisterVisitorCommand());
+		commandRegistry.registerCommand(new RegisterMinorVisitorCommand());
+	}
+
+	public void waitForInput()
+	{
+		boolean shouldExit = false;
+		while (!shouldExit)
+		{
+			System.out.print(">> ");
+			Scanner scanner = new Scanner(System.in);
+			String command = scanner.nextLine();
+			execute(command);
+		}
+	}
+
+	public void help()
+	{
+		System.out.println(commandRegistry.prettyPrint());
+	}
+
+	public void execute(@NotNull String command)
+	{
+		String[] tokens = command.split(" ");
+
+		if (tokens.length == 0)
+		{
+			System.err.println("No command provided");
+		}
+
+		try
+		{
+			commandRegistry.run(tokens[0], tokens.length > 1 ? Arrays.stream(tokens).skip(1).toArray(String[]::new) : new String[0], this);
+		}
+		catch (CommandNotFoundException e)
+		{
+			System.err.printf("Command not found: %s%n", e.getMessage());
+		}
+		catch (CommandException e)
+		{
+			System.err.printf("Error running command: %s%n", e.getMessage());
+		}
+	}
+
+	private final CommandRegistry commandRegistry = new CommandRegistry();
+}
