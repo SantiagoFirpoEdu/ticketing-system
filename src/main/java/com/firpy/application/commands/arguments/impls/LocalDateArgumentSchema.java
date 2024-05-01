cbaf1b331035b7ee9be4cfe2790fb755c2dddb10
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2024-05-01 15:44:34 -0300

    Implemented LocalDate argument schema

---
+++ src/main/java/com/firpy/application/commands/arguments/impls/LocalDateArgumentSchema.java
@@ -0,0 +1,33 @@
+package com.firpy.application.commands.arguments.impls;
+
+import com.firpy.application.commands.arguments.ArgumentSchema;
+import com.firpy.application.commands.exceptions.CommandException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class LocalDateArgumentSchema extends ArgumentSchema
+{
+    public LocalDateArgumentSchema(String name, String description)
+    {
+        super(name, "%s. Format is %s".formatted(description, DATE_PATTERN));
+    }
+
+    @Override
+    public LocalDate parse(String[] args) throws CommandException
+    {
+        checkSize(args);
+        String arg = args[getIndex()];
+        try
+        {
+            return LocalDate.parse(arg, DateTimeFormatter.ofPattern(DATE_PATTERN));
+        }
+        catch (DateTimeParseException e)
+        {
+            throw new CommandException("Invalid date format: %s".formatted(arg));
+        }
+    }
+
+    private static final String DATE_PATTERN = "dd-MM-yyyy";
+}
