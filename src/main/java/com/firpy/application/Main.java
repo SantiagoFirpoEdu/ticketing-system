1319da713722cd55c8df40c4147f775ae118b14a
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-05-25 16:25:03 -0300

    Mocked visits and used unique id for visits

--- src/main/java/com/firpy/application/Main.java
+++ src/main/java/com/firpy/application/Main.java
@@ -4,8 +4,10 @@ import com.firpy.application.commands.impls.*;
 import com.firpy.application.shell.Shell;
 import com.firpy.model.*;
 import com.firpy.repositories.CrudRepository;
-import com.firpy.repositories.CrudValueRepository;
 import com.firpy.repositories.impls.VisitorDataAccess;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDate;
 
 public class Main
 {
@@ -15,11 +17,10 @@ public class Main
         CrudRepository<AdultVisitor, Long> visitorRepository = new CrudRepository<>();
         CrudRepository<Attraction, Long> attractionRepository = new CrudRepository<>();
         CrudRepository<Ticket, TicketId> ticketRepository = new CrudRepository<>();
-        CrudValueRepository<Visit> visitRepository = new CrudValueRepository<>();
-        attractionRepository.save(new Attraction(0, "Montanha Russa"));
-        attractionRepository.save(new Attraction(1, "Roda Gigante"));
-        attractionRepository.save(new Attraction(2, "Barco Pirata"));
-        attractionRepository.save(new Attraction(3, "Carro Bate Bate"));
+        CrudRepository<Visit, Long> visitRepository = new CrudRepository<>();
+
+        mockAttractionData(attractionRepository);
+        mockVisitData(visitRepository);
 
         VisitorDataAccess visitorDataAccess = new VisitorDataAccess(visitorRepository, minorVisitorRepository);
         Shell shell = new Shell
@@ -27,10 +28,29 @@ public class Main
             new RegisterVisitorCommand(visitorDataAccess),
             new RegisterMinorVisitorCommand(visitorDataAccess),
             new ListVisitors(visitorDataAccess),
+            new QueryVisitsByDateCommand(visitRepository, attractionRepository),
             new ExitCommand(),
             new HelpCommand()
         );
         shell.help();
         shell.waitForInput();
+    }
+
+    private static void mockAttractionData(@NotNull CrudRepository<Attraction, Long> attractionRepository)
+    {
+        attractionRepository.save(new Attraction(0, "Montanha Russa"));
+        attractionRepository.save(new Attraction(1, "Roda Gigante"));
+        attractionRepository.save(new Attraction(2, "Barco Pirata"));
+        attractionRepository.save(new Attraction(3, "Carro Bate Bate"));
+    }
+
+    private static void mockVisitData(@NotNull CrudRepository<Visit, Long> visitRepository)
+    {
+        visitRepository.save(new Visit(0, 0, new TicketId(LocalDate.now(), 0), LocalDate.now(), 0L));
+        visitRepository.save(new Visit(1, 0, new TicketId(LocalDate.now(), 0), LocalDate.now(), 0L));
+        visitRepository.save(new Visit(2, 0, new TicketId(LocalDate.now(), 0), LocalDate.now(), 1L));
+        visitRepository.save(new Visit(3, 0, new TicketId(LocalDate.now(), 0), LocalDate.now(), 1L));
+        visitRepository.save(new Visit(4, 0, new TicketId(LocalDate.now(), 0), LocalDate.now(), 1L));
+        visitRepository.save(new Visit(5, 0, new TicketId(LocalDate.now(), 0), LocalDate.now(), 2L));
     }
 }
