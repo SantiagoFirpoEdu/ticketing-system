24031f553983a5abde7510bcf7b963dfa3be4fb6
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-06-03 23:35:27 -0300

    Used the commands API instead of manual parsing

--- src/main/java/com/firpy/application/commands/impls/EmitTicketCommand.java
+++ src/main/java/com/firpy/application/commands/impls/EmitTicketCommand.java
@@ -5,21 +5,16 @@ import com.firpy.application.commands.arguments.impls.LongArgumentSchema;
 import com.firpy.application.commands.exceptions.CommandException;
 import com.firpy.application.commands.exceptions.CommandUsageException;
 import com.firpy.application.shell.Shell;
+import com.firpy.model.Visitor;
 import com.firpy.repositories.exceptions.DailyTicketLimitReachedException;
 import com.firpy.repositories.impls.TicketDataAccess;
 import com.firpy.repositories.impls.VisitorDataAccess;
 import org.jetbrains.annotations.NotNull;
 import java.time.LocalDate;
-
-import java.time.LocalDate;
+import java.util.Optional;
 
 public class EmitTicketCommand extends Command
 {
-	private final TicketDataAccess ticketDataAccess;
-	private final VisitorDataAccess visitorDataAccess;
-	private final LongArgumentSchema visitorIdArgument = new LongArgumentSchema("visitor-id", "Visitor ID to emit a ticket for.", this);
-	private final LocalDateArgumentSchema purchaseDateArgument = new LocalDateArgumentSchema("purchase-date", "Purchase date of the ticket.", this);
-
 	public EmitTicketCommand(TicketDataAccess ticketDataAccess, VisitorDataAccess visitorDataAccess)
 	{
 		super("emit-ticket", "Emit a ticket for a visitor.");
@@ -31,30 +26,28 @@ public class EmitTicketCommand extends Command
 	@Override
 	public void run(@NotNull String[] args, @NotNull Shell shell) throws CommandException, CommandUsageException
 	{
-	
-		if (args.length != 1) {
-			throw new CommandUsageException("Usage: emit-ticket <visitor-id>",this);
-		}
+		long visitorId = visitorIdArgument.parse(args);
+		LocalDate ticketDate = purchaseDateArgument.parse(args);
 
-		Long visitorId;
-		try {
-			visitorId = Long.parseLong(args[0]);
-		} catch (NumberFormatException e) {
-			throw new CommandUsageException("Visitor ID must be a number.",this);
-		}
-
-		var visitor = visitorDataAccess.findVisitorById(visitorId);
-		if (visitor.isEmpty()) {
+		Optional<Visitor> visitor = visitorDataAccess.findVisitorById(visitorId);
+		if (visitor.isEmpty())
+		{
 			throw new CommandException("Visitor with ID " + visitorId + " not found.");
 		}
 
-		try {
-			ticketDataAccess.registerTicket(LocalDate.now(), visitor.get());
-			shell.println("Ticket emitted for visitor " + visitor.get().getName());
-		} catch (DailyTicketLimitReachedException e) {
-			shell.println("Daily ticket limit reached. Try again tomorrow.");
+		try
+		{
+			ticketDataAccess.registerTicket(ticketDate, visitor.get());
+			shell.println("Ticket emitted for visitor %s".formatted(visitor.get().getName()));
+		}
+		catch (DailyTicketLimitReachedException e)
+		{
+			throw new CommandException(e.getMessage());
 		}
 	}
 
-
+	private final TicketDataAccess ticketDataAccess;
+	private final VisitorDataAccess visitorDataAccess;
+	private final LongArgumentSchema visitorIdArgument = new LongArgumentSchema("visitor-id", "Visitor ID to emit a ticket for.", this);
+	private final LocalDateArgumentSchema purchaseDateArgument = new LocalDateArgumentSchema("purchase-date", "Purchase date of the ticket.", this);
 }
